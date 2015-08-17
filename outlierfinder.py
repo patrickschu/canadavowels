fa4de04f43c14f4ade416e375f87763291d7802e
patrickschu <patrickschu@users.noreply.github.com>
2015-08-17 13:57:25 -0500

    scripts for means and devs and finding outliers

--- outlierfinder.py
+++ outlierfinder.py
@@ -1,5 +1,3 @@
-#find outliers
-
 import codecs
 import numpy
 
@@ -10,7 +8,8 @@ for line in f:
 	#print line.rstrip("\r").split(",")
 	spreadsheet.append(line.rstrip("\r").split(","))
 	
-print len(spreadsheet)	
+print len(spreadsheet)
+f.close()	
 
 #>>> spreadsheet[0][6]
 #u'F2'
@@ -21,18 +20,67 @@ print len(spreadsheet)
 #>>> spreadsheet[0][14]
 #u'F2n'
 
-def outlierfinder(vowel):
-	f1=[]
-	f2=[]
+def meansanddevs(vowel):
+	f1list=[]
+	f2list=[]
 	for entry in spreadsheet:
 		if vowel in entry:
 			#print entry[14], entry[13]
-			f1.append(float(entry[13]))
+			f1=float(entry[13])
+			f1list.append(f1)
 			#print f1
-			f2.append(float(entry[14]))
-	standarddeviationf1=numpy.std(f1)
-	standarddeviationf2=numpy.std(f2)
+			f2=float(entry[14])
+			f2list.append(f2)
+	standarddeviationf1=numpy.std(f1list)
+	meanf1=numpy.mean(f1list)
+	standarddeviationf2=numpy.std(f2list)
+	meanf2=numpy.mean(f2list)
 	print "stdev f1n:", standarddeviationf1
 	print "stdev f2n", standarddeviationf2
+	print "mean f1", meanf1
+	print "mean f2", meanf2
+	return(meanf1, standarddeviationf1, meanf2, standarddeviationf2)
+	print "mean f1", meanf1
+	print "mean f2", meanf2
 			
-outlierfinder("DRESS")
+x=meansanddevs("DRESS")
+#print x
+
+#we take the stdevs and rund them past every freaking measurement in the spreadsheet
+def outlierfinder(vowel, (meanf1, standarddeviationf1, meanf2, standarddeviationf2)):
+	linecount=1
+	for entry in spreadsheet[1:len(spreadsheet)]:
+		linecount=linecount+1
+		alarms=[]
+		if vowel in entry:
+			f1=float(entry[13])
+			f2=float(entry[14])
+			#print f1
+			#print meanf1-(2*standarddeviationf1)
+			#print meanf1+(2*standarddeviationf1)
+			if f1 < meanf1-(2*standarddeviationf1) or f1 > meanf1+(2*standarddeviationf1):
+				print entry
+				alarms.append(("F1 problem", linecount, entry))
+			else:
+				pass
+			if f2 < meanf2-(2*standarddeviationf2) or f2 > meanf2+(2*standarddeviationf2):
+				print entry
+				alarms.append(("F2 problem", linecount, entry))
+				print alarms
+			else:
+				pass
+	return alarms
+	print linecount
+
+outlierfinder("DRESS", x)
+
+def outlierkiller(vowel):
+	x=meansanddevs(vowel)
+	alarms=outlierfinder(vowel, x)
+	return alarms
+	#print alarms
+	
+#y=outlierkiller("DRESS")
+
+#print y[0]
+	
