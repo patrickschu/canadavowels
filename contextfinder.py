6e5c70f415ae2ea2ba1e77a0b34f9bbcf55f57c1
patrickschu <patrickschu@users.noreply.github.com>
2015-08-20 11:09:01 -0500

    almost done

--- contextfinder.py
+++ contextfinder.py
@@ -2,11 +2,13 @@
 # Identify word and vowel
 # Look at vowel context
 
-import nltk; import codecs; import os; import re
+import nltk; import codecs; import os; import re; import numpy; from collections import defaultdict
 from nltk.corpus import cmudict
 
 
+
 def cmureader(filename):
+    output=defaultdict(list)
     #this counter just for testing purposes
     count=0
     #getting the dictionary from NLTK
@@ -24,23 +26,36 @@ def cmureader(filename):
         try:
             #if several entries, we just take the first one
             #none of the words has secondary stress
-            x=cmudict[word][0]
+            transcription=cmudict[word][0]
+            #print x
             #find the primestressvowel and the two sounds next to it
-            context=vowelfinder(x)
-            print(len(context))
+            indexi=vowelfinder(transcription)
+            context=[transcription[i] for i in indexi if i < len(transcription)]
+            #print context
+            #y=x[context]
             #print context
         except:
+            print transcription
+            print word
             print "error"
-    print count
+            break
+        output[int(entry[0])]=[entry, transcription, [list(i) for i in context]]
+    print len(output)
+    print output[2]
+    return output
+    
+
+#cmudict["bed"]
+#[[u'B', u'EH1', u'D']]
 
 def vowelfinder(transcription):
-    #this makes a list of primary stress vowels based on the vowels list above
-    primestressvowels=[str(i+"1") for i in vowels]
     #these are the vowels in the CMU dictionary
     vowels=["AA", "AH", "AW", "EH", "EY", "IH", "OW", "UH", "AE", "AO", "AY", "ER", "IY", "OY", "UW"]
-    
+    #this makes a list of primary stress vowels based on the vowels list above
+    primestressvowels=[str(i+"1") for i in vowels]
     for sound in transcription:
         if sound in primestressvowels:
-            return (transcription.index(sound)-1, transcription.index(sound)+1)
+            #print sound
+            return [transcription.index(sound)-1, transcription.index(sound)+1]
 
-cmureader("canada_outliers_817.csv")
+t=cmureader("canada_outliers_817.csv")
