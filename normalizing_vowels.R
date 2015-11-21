6e67ae8dba9e96524e7968d9a5bae792ca1b487e
patrickschu <patrickschu@users.noreply.github.com>
2015-11-21 12:31:13 -0600

    Update normalizing_vowels.R

--- normalizing_vowels.R
+++ normalizing_vowels.R
@@ -5,7 +5,7 @@ library(vowels)
 
 can=read.csv("H:/canada_outliers_cmuvowels_gormancategories_929.csv", header=T)
 
-
+#note that this works with the initial dataset only. once we get rid of the CONTEXT columns, use code below
 vowelplotmaker=function(dataset){
 vowels=dataset[,c("SPEAKER", "VOWEL", "CONTEXT", "F1", "F2", "F3",
 "CONTEXT", "CONTEXT", "CONTEXT")]
@@ -29,3 +29,16 @@ can$F1l=g[,4]
 can$F2l=g[,5]
 
 summary(can)
+
+
+
+#this one works for later datasets
+vowelplotmaker=function(dataset){
+vowels=dataset[,c("SPEAKER", "VOWEL", "TOKEN", "F1", "F2", "F3",
+"TOKEN", "TOKEN", "TOKEN")]
+vowels$TOKEN=NA
+vowels$TOKEN.1=NA
+vowels$TOKEN.2=NA
+vowels$TOKEN.3=NA
+return(vowels)
+}
